63f74181bbcce54c9d91e884a6c8738d4dad2c14
hvpulok <hvpulok@gmail.com>
2019-10-27 21:18:32 -0400

    Update demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-api-server-interceptor-demo';
+  subtitle = `${environment['apiServer']}`;
+  dataFromServer$ = this.http.get('/todos/1');
+
+  constructor(private http: HttpClient) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgApiServerInterceptor } from '@muminlife/ng-api-server-interceptor';
+
+export function ngApiServerInterceptorFactory() {
+  return new NgApiServerInterceptor(environment['apiServer']);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useFactory: ngApiServerInterceptorFactory,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
